fc3188835195382febfdd289b42cb242841e79ff
oberrow <72581470+oberrow@users.noreply.github.com>
2022-01-28 18:45:16 -0500

    Create javascript.js
    Makes a "Button" function

---
+++ docs/source/javascript.js
@@ -0,0 +1,16 @@
+function button(var toHref, var action, var quirk) {
+  if(quirk = 1)
+  {
+    <form action="https://github.com/oberrow/cmd-game/releases/download/PRE-R-v1.1.5/cmd-game.exe">
+            <input id="download" type="submit" value="Click me to download cmd-game.exe's latest pre release!" />
+    </form>
+    <br>
+    <br>
+  }
+  else {
+   <form action="https://github.com/oberrow/cmd-game/releases/download/PRE-R-v1.1.5/cmd-game.exe">
+            <input id="download" type="submit" value="Click me to download cmd-game.exe's latest pre release!" />
+        </form>
+   <br> 
+  }
+}
